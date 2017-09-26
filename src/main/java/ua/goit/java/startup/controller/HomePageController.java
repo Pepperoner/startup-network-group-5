d9b6869e247feb7dced9eafcea618e4977a88641
Aleksandr Bednyi <nevergiveupman9@gmail.com>
2017-09-26 16:27:33 +0300

    Create HomePageController task, in process

---
+++ src/main/java/ua/goit/java/startup/controller/HomePageController.java
@@ -0,0 +1,13 @@
+package ua.goit.java.startup.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Aleksandr on 26.09.2017.
+ */
+@Controller
+@ComponentScan(basePackages = "ua.goit.java.startup.domaiservise")
+public class HomePageController {
+
+}
