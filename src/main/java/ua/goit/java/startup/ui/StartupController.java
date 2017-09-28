0c50c4ef20472115dab564123d7d5b1718dcff94
dmoroz <avangardi@ukr.net>
2017-09-28 14:24:09 +0300

    StartupController changes

--- src/main/java/ua/goit/java/startup/ui/StartupController.java
+++ src/main/java/ua/goit/java/startup/ui/StartupController.java
@@ -15,48 +15,48 @@ import ua.goit.java.startup.dto.DtoStartup;
 @Controller
 public class StartupController {
 
-    private StartupService startupService;
-    private UserService userService;
+    private final StartupService startupService;
+    private final UserService userService;
 
-//    @Autowired
-//    public StartupController(StartupService startupService, UserService userService) {
-//        this.startupService = startupService;
-//        this.userService = userService;
-//    }
-//
-//    @RequestMapping(method = RequestMethod.GET)
-//    public ResponseEntity<DtoStartup> getStartupById(@PathVariable("id") long id) {
-//        DtoStartup dtoStartup = startupService.getDtoStartupById(id);
-//        if (dtoStartup != null) {
-//            return new ResponseEntity<DtoStartup>(dtoStartup, HttpStatus.OK);
-//        }
-//        return new ResponseEntity<DtoStartup>(HttpStatus.FORBIDDEN);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.PUT)
-//    public ResponseEntity<DtoStartup> createStartup(@RequestBody DtoStartup dtoStartup) {
-//        dtoStartup = startupService.createNewStartup(dtoStartup);
-//        if (dtoStartup != null) {
-//            return new ResponseEntity<DtoStartup>(dtoStartup, HttpStatus.OK);
-//        }
-//        return new ResponseEntity<DtoStartup>(HttpStatus.FORBIDDEN);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.PUT)
-//    public ResponseEntity<DtoStartup> updateStartup(@RequestBody DtoStartup dtoStartup) {
-//        dtoStartup = startupService.saveDtoStartup(dtoStartup);
-//        if (dtoStartup != null) {
-//            return new ResponseEntity<DtoStartup>(dtoStartup, HttpStatus.OK);
-//        }
-//        return new ResponseEntity<DtoStartup>(HttpStatus.FORBIDDEN);
-//    }
-//
-//    @RequestMapping(method = RequestMethod.DELETE)
-//    public ResponseEntity<DtoStartup> deleteStartup(@PathVariable("id") long id) {
-//        DtoStartup dtoStartup = startupService.deleteStartup(id);
-//        if (dtoStartup!=null){
-//            return new ResponseEntity<DtoStartup>(dtoStartup, HttpStatus.OK);
-//        }
-//        return new ResponseEntity<DtoStartup>(HttpStatus.FORBIDDEN);
-//    }
+    @Autowired
+    public StartupController(StartupService startupService, UserService userService) {
+        this.startupService = startupService;
+        this.userService = userService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Startup> getStartupById(@PathVariable("id") long id) {
+        Startup startup = startupService.getStartupById(id);
+        if (startup != null) {
+            return new ResponseEntity<Startup>(startup, HttpStatus.OK);
+        }
+        return new ResponseEntity<Startup>(HttpStatus.FORBIDDEN);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Startup> createStartup(@RequestBody Startup startup) {
+        startup = startupService.createNewStartup(startup);
+        if (startup != null) {
+            return new ResponseEntity<Startup>(startup, HttpStatus.OK);
+        }
+        return new ResponseEntity<Startup>(HttpStatus.FORBIDDEN);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Startup> updateStartup(@RequestBody Startup startup) {
+        startup = startupService.saveStartup(startup);
+        if (startup != null) {
+            return new ResponseEntity<Startup>(startup, HttpStatus.OK);
+        }
+        return new ResponseEntity<Startup>(HttpStatus.FORBIDDEN);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Startup> deleteStartup(@PathVariable("id") long id) {
+        Startup startup = startupService.deleteStartup(id);
+        if (startup!=null){
+            return new ResponseEntity<Startup>(startup, HttpStatus.OK);
+        }
+        return new ResponseEntity<Startup>(HttpStatus.FORBIDDEN);
+    }
 }
