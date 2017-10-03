e118de043978f8875fdc2497e6555a66a175f27b
Aleksandr Bednyi <nevergiveupman9@gmail.com>
2017-10-03 16:17:42 +0300

    add StartupTranslator and StartupAspect

---
+++ src/main/java/ua/goit/java/startup/aspect/StartupAspect.java
@@ -0,0 +1,30 @@
+package ua.goit.java.startup.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+import ua.goit.java.startup.bom.Startup;
+import ua.goit.java.startup.dao.StartupDTORepository;
+import ua.goit.java.startup.dto.StartupDto;
+import ua.goit.java.startup.translator.StartupTranslator;
+
+@Aspect
+@Component
+public class StartupAspect {
+
+    private final StartupDTORepository startupDTORepository;
+    private final StartupTranslator startupTranslator;
+
+    public StartupAspect(StartupDTORepository startupDTORepository, StartupTranslator startupTranslator) {
+        this.startupDTORepository = startupDTORepository;
+        this.startupTranslator = startupTranslator;
+    }
+
+    @Before("execution(* ua.goit.java.startup.bom.Startup.get*(..))")
+    public void fillStarupInfo(JoinPoint joinPoint) {
+        Startup startup = (Startup) joinPoint.getTarget();
+        StartupDto startupDto = startupDTORepository.findOne(startup.getId());
+        startupTranslator.fromDTO(startupDto, startup);
+    }
+}
--- src/main/java/ua/goit/java/startup/translator/StartupTranslator.java
+++ src/main/java/ua/goit/java/startup/translator/StartupTranslator.java
@@ -1,9 +1,6 @@
 package ua.goit.java.startup.translator;
 
-import ua.goit.java.startup.bom.Developer;
-import ua.goit.java.startup.bom.Model;
-import ua.goit.java.startup.bom.Startup;
-import ua.goit.java.startup.bom.UserRole;
+import ua.goit.java.startup.bom.*;
 import ua.goit.java.startup.dto.StartupDto;
 import ua.goit.java.startup.dto.UserDto;
 
@@ -12,8 +9,8 @@ import java.util.Set;
 
 public class StartupTranslator {
 
-    public void fromDTO(StartupDto source, Model destination){
-        if (source == null){
+    public void fromDTO(StartupDto source, Model destination) {
+        if (source == null) {
             return;
         }
 
@@ -27,18 +24,48 @@ public class StartupTranslator {
         Set<Developer> developers = new HashSet<>();
         startupDestination.setDeveloper(developers);
         for (UserDto userDto : source.getUserDto()) {
-            if(UserRole.DEVELOPER.equals(userDto.getRole())) {
+            if (UserRole.DEVELOPER.equals(userDto.getRole())) {
                 Developer developer = new Developer();
                 developer.setId(userDto.getId());
                 developers.add(developer);
             }
         }
-
-//        startupDestination.setDeveloper(source.getUserDto().stream().filter(user -> UserRole.DEVELOPER.equals(user.getRole())).map(userDto -> ));
-//        startupDestination.setDeveloper(source.getUserDto().forEach(dev -> dev.getRole().equals(UserRole.DEVELOPER)));
-//        startupDestination.setInvestor(source.getUserDto().forEach(inv -> inv.getRole().equals(UserRole.INVESTOR)));
+        Set<Investor> investors = new HashSet<>();
+        startupDestination.setInvestor(investors);
+        for (UserDto userDto : source.getUserDto()) {
+            if (UserRole.INVESTOR.equals(userDto.getRole())) {
+                Investor investor = new Investor();
+                investor.setId(userDto.getId());
+                investors.add(investor);
+            }
+        }
     }
 
+    public void toDTO(Startup source, StartupDto destination) {
+        if (source == null) {
+            return;
+        }
+
+        destination.setId(source.getId());
+        destination.setName(source.getName());
+        destination.setDescription(source.getDescription());
+        destination.setCost(source.getCost());
+        destination.setCurrentsum(source.getCurrentsum());
+        destination.setImage(source.getImage());
+        Set<UserDto> userDtos = new HashSet<>();
+        destination.setUserDto(userDtos);
+        for (Developer developer : source.getDeveloper()) {
+            UserDto userDto = new UserDto();
+            userDto.setId(developer.getId());
+            userDtos.add(userDto);
+        }
+        for (Investor investor : source.getInvestor()) {
+            UserDto userDto = new UserDto();
+            userDto.setId(investor.getId());
+            userDtos.add(userDto);
+        }
+    }
+}
 //    public Set<Startup> getSetFromDTO(Set<StartupDto> dtoSet){
 //        Set<Startup> startupSet = new HashSet<>();
 //        for (StartupDto startupDto : dtoSet) {
@@ -47,4 +74,4 @@ public class StartupTranslator {
 //        }
 //    }
 
-}
+
