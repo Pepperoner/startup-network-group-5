2cc3c0d0292dd34290c93ea3e278654f83e787de
dmoroz <avangardi@ukr.net>
2017-10-14 11:28:07 +0300

    UserDetailServiceImpl

--- src/main/java/ua/goit/java/startup/domainservice/impl/UserDetailServiceImpl.java
+++ src/main/java/ua/goit/java/startup/domainservice/impl/UserDetailServiceImpl.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class UserDeteilServiceImpl implements UserDetailsService {
+public class UserDetailServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
